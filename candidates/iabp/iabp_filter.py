b710eed710960b129648f108371d4a94cef2e353
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2026-01-09 15:58:52 +0000

    begin to score model output

--- candidates/iabp/iabp_filter.py
+++ candidates/iabp/iabp_filter.py
@@ -21,20 +21,23 @@ def hasta(fwords):
     ''' hastafwords) -- fwords is line.split() of the buoy's header line '''
     return  'Ta' in fwords
 
-def nearcycle(fcycle, fincr):
-  ''' nearcycle(cycle, fincr) checks to see if fincr (increment since 
+# Check whether the data are 'near' the given hour
+def nearcycle(fcycle, fincr, fnear):
+  ''' nearcycle(cycle, fincr, fnear) checks to see if fincr (increment since 
       start of day) is 'near' the cycle time (hours since 00 UTC)
-      cycle is hours, fincr is a timedelta. 
-      RG: 'near' should be an argument. Taken as 30 min for now
-      RG: Does not currently deal with wrapping across 00 UTC, 
-        e.g. 2359 won't be 'near' 0000 '''
-  return (abs(fincr.total_seconds()%86400 - fcycle*3600) <= 1800)
+      cycle is hours, fincr is a timedelta, 
+      fnear is the tolerance in time for being 'near' 
+      RG: logic on 'near' is not well-tested
+  '''
+  return (abs(fincr.total_seconds()%86400 - fcycle*3600) <= fnear or \
+          abs(86400-fincr.total_seconds() - fcycle*3600) <= fnear )
 
 #-------------------------------------------------------------------------
 # Change these
 start = datetime.date(2025,1,1)
-end   = datetime.date(2025,5,18)
+end   = datetime.date(2026,1,8)
 cycle = 0 # hours UTC
+dtlim = 60*30 # window in seconds of being 'near' cycle
 
 #-------------------------------------------------------------------------
 # Should need no changes below here
@@ -52,19 +55,26 @@ except:
 # Read and echo the header line
 try:
     line = fin.readline()
-    print(line,end="")
+    #debug: print(line,end="", flush=True)
     # The additional variables are not being used at this point, this is a preadaptation
     # BP is buoy pressure, Ts is surface (skin) temperature, Ta is atmospheric temperature
-    words = line.split()
-    if hasbp(words):
-        print('buoy ',sys.argv[1], 'has BP in index position ',words.index('BP'), file=sys.stderr )
-        BP = words.index('BP')
-    if hasts(words):
-        print('buoy ',sys.argv[1], 'has Ts in index position ',words.index('Ts'), file=sys.stderr )
-        Ts = words.index('Ts')
-    if hasta(words):
-        print('buoy ',sys.argv[1], 'has Ta in index position ',words.index('Ta'), file=sys.stderr )
-        Ta = words.index('Ta')
+    hwords = line.split()
+    usebp = False
+    usets = False
+    useta = False
+    if hasbp(hwords):
+        print('buoy ',sys.argv[1], 'has BP in index position ',hwords.index('BP'), file=sys.stderr )
+        BP = hwords.index('BP')
+        usebp = True
+    if hasts(hwords):
+        print('buoy ',sys.argv[1], 'has Ts in index position ',hwords.index('Ts'), file=sys.stderr )
+        Ts = hwords.index('Ts')
+        usets = True
+    if hasta(hwords):
+        print('buoy ',sys.argv[1], 'has Ta in index position ',hwords.index('Ta'), file=sys.stderr )
+        Ta = hwords.index('Ta')
+        useta = True
+
 except:
     print("readline failed",line)
     sys.exit(1)
@@ -73,7 +83,7 @@ except:
 for more in fin:
   words = more.split()
   #debug: print(words[1], words[5], words[6], words[7], flush=True )
-  if (float(words[6]) <= -90. or float(words[6]) >= 90.0):
+  if (float(words[6]) <= -90. or float(words[6]) >= 90.0 or float(words[6]) == 0):
       continue
   lon = float(words[7])
   if (lon < -180):
@@ -82,10 +92,17 @@ for more in fin:
       lon -= 360.
   lat = float(words[6])
 
-  #RG: Add logic to determine whether we're near a given 'cycle' time, e.g. 00z
   obs = datetime.date(int(words[1]), 1, 1)
   incr = (float(words[5])-1.) * dt
   obs += incr
-  #if (obs >= start and obs <= end and nearcycle(cycle, incr) ):
-  if (end >= obs >= start and nearcycle(cycle, incr) ):
-    print(words[1], words[5], lat, lon, flush=True)
+
+  # RG: preserve previous and only take new days' observations, vs. multiple within the 2*dtlim window
+  if (end >= obs >= start and nearcycle(cycle, incr, dtlim) ):
+    print(words[1], words[5], lat, lon, end=" " )
+    if (usebp):
+        print(f"BP: {float(words[BP]):.2f}",end=" ")
+    if (useta):
+        print(f"Ta: {float(words[Ta]):.2f}",end=" ")
+    if (usets):
+        print(f"Ts: {float(words[Ts]):.2f}",end=" ")
+    print("", flush=True)
--- candidates/iabp/iabp_speed.py
+++ candidates/iabp/iabp_speed.py
@@ -19,37 +19,18 @@ from utility import harcdis, bearing
 import latpt
 
 #-----------------------------------------------------------------
-## to add to utility
-#def bearing(x1, x2):
-#    dlon = x2.lon - x1.lon
-#    # must change to radians
-#    rpdg = pi/180.
-#    theta = atan2(sin(dlon*rpdg)*cos(x2.lat*rpdg) ,
-#                  cos(x1.lat*rpdg)*sin(x2.lat*rpdg) -
-#                  sin(x1.lat*rpdg)*cos(x2.lat*rpdg)*cos(dlon*rpdg) )
-#    return theta/rpdg
-
+# Change these to suit
+splim = 1.0   # fastest allowed ice drift, m/s
+dtlim = 1800. # minimum time separation between obs, s
 #-----------------------------------------------------------------
-# format is header line and then N lines of data. Position DOY may not match obs DOY
-start = datetime.datetime(2025,1,1)
-end   = datetime.datetime(2025,4,23)
-splim = 1.0   # fastest allowed ice drift
-dtlim = 1800. # minimum time separation between obs
-#-----------------------------------------------------------------
-
 # Open file
+# Internal format is N lines of data, possibly trailing BP, Ts, Ta
 try:
   fin = open(sys.argv[1], "r", encoding='utf-8')
 except:
   print("could not open ",sys.argv[1])
   sys.exit(1)
 
-# Read and echo the header line
-try:
-    line = fin.readline()
-except:
-    print("readline failed")
-    sys.exit(1)
 #-----------------------------------------------------------------
 
 dt  = datetime.timedelta(1)
@@ -59,18 +40,14 @@ for more in fin:
   obs = datetime.datetime(int(words[0]), 1, 1)
   obs += (float(words[1])-1.) * dt
   position = ( float(words[2]), float(words[3]) )
-  #print(position)
+
   if (k == 0):
     prev_obs = obs
     prev_position = position
   else:
-    #RG: watch this for days vs hours, monotonicity, ...
     delta = (obs - prev_obs).total_seconds()
-    dlat = position[0]-prev_position[0]
-    dlon = position[1]-prev_position[1]
 
     if (obs > prev_obs):
-      #speed = calculate_distance(position, prev_position) /delta
       p1 = latpt.latpt(position[0], position[1])
       p2 = latpt.latpt(prev_position[0], prev_position[1])
       dist  = harcdis(p1, p2)*1000. # harcdis is in km
@@ -80,11 +57,12 @@ for more in fin:
       speed = 0.
       direction = 0.
 
-    if (dlat != 0 and dlon != 0 and delta >= 3600 and speed <= splim):
-      print(f"{k:5d}", obs.strftime("%Y%m%d"), obs.strftime("%H%M%S"), \
-            f"{delta:5.0f}", f"{dist:7.2f}", f"{direction:.5f}", f"{speed:.5f}" )
-    elif (speed > splim):
-      print(k,'zzz',position, prev_position, delta, dlat, dlon, speed)
+    if (delta >= dtlim and speed <= splim):
+      print(f"{k:5d}", prev_obs.strftime("%Y%m%d"), prev_obs.strftime("%H%M%S"), \
+            f"{p2.lat:.5f}", f"{p2.lon:.5f}",
+            f"{delta:5.0f}", f"{dist:8.2f}", f"{direction:.5f}", f"{speed:.5f}" )
+    elif (speed > splim and delta >= dtlim):
+      print(k,position, prev_position, delta, dist, direction, speed, file=sys.stderr)
 
     prev_position = position
     prev_obs      = obs
